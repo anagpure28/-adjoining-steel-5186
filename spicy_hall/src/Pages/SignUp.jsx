cce35e75c9facd81b77910c71a56f94eefa240e8
anagpure28 <aniketnagpureofficial@gmail.com>
2023-07-21 00:12:55 +0530

    bugs fixed on home & Signup

--- spicy_hall/src/Pages/SignUp.jsx
+++ spicy_hall/src/Pages/SignUp.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import "../Styles/Signup.css"
 
 const SignUp = () => {
   return (
-    <div>
-      
+    <div className='signup-main'>
+      <div className='signup-cont'>
+        <form action="">
+          <label htmlFor="Username"></label>
+          <br />
+          <input type="text" name="" placeholder='Username'/>
+          <br />
+          <label htmlFor="Username"></label>
+          <br />
+          <input type="text" name="" placeholder='Username'/>
+          <br />
+          <label htmlFor="Username"></label>
+          <br />
+          <input type="text" name="" placeholder='Username'/>
+          <br />
+          <label htmlFor="Username"></label>
+          <br />
+          <input type="text" name="" placeholder='Username'/>
+          <br />
+          <label htmlFor="Username"></label>
+          <br />
+          <input type="text" name="" placeholder='Username'/>
+          <br />
+        </form>
+      </div>
     </div>
   )
 }
