1eeaafdf24acc8e92187c0f9be20a0852bd481a8
anagpure28 <aniketnagpureofficial@gmail.com>
2023-07-22 21:07:55 +0530

    User Login done

--- spicy_hall/src/Pages/Login.jsx
+++ spicy_hall/src/Pages/Login.jsx
@@ -3,31 +3,61 @@ import "../Styles/Signup.css";
 import img4 from "../Images/Home/img4.jpg";
 import { Parallax } from "react-parallax";
 import { Link, useNavigate } from "react-router-dom";
+import { useToast } from "@chakra-ui/react";
+import {url} from "../Url"
+import axios from "axios";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const navigate = useNavigate()
+  const toast = useToast();
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const payload = {email, password };
-    fetch("https://spicy-hall.onrender.com/users/login",{
-          method: "POST",
-          headers: {
-              "Content-Type": "application/json"
-          },
-          body: JSON.stringify(payload)
-      }).then(res=> res.json())
-      .then(res=> {
-        console.log(res)
-        localStorage.setItem("token", res.token)
-      })
-      .catch(err=> console.log(err))
-      setEmail("")
-      setPassword("")
+    e.preventDefault();
+    const payload = { email, password };
+    if (email && password) {
+      axios
+        .post(`${url}/users/login`, payload)
+        .then((res) => {
+          console.log(res, "LOGIN");
+          if (res.status === 200) {
+            localStorage.setItem("token", res.data.token);
+            setTimeout(() => {
+              navigate("/");
+            }, 2000);
+          }
+          toast({
+            title: res.statusText,
+            description: res.data.msg,
+            status: res.data.status,
+            duration: 6000,
+            isClosable: true,
+            position: "top",
+            colorScheme: "green"
+          });
+        })
+        .catch((err) => {
+          console.log(err)
+          toast({
+            description: "Wrong Credentials",
+            status: "error",
+            duration: 6000,
+            isClosable: true,
+            position: "top",
+          });
+        });
+    } else {
+      toast({
+        // title: '',
+        description: "Please fill all fields",
+        status: "error",
+        duration: 6000,
+        isClosable: true,
+        position: "top",
+      });
     }
-
+  };
 
   return (
     <div style={{ backgroundColor: "black" }}>
@@ -55,10 +85,15 @@ const Login = () => {
             <br />
             <hr />
             <br />
-            <button className="submit2" onClick={handleSubmit}>Login</button>
+            <button className="submit2" onClick={handleSubmit}>
+              Login
+            </button>
             <br />
             <p className="p2">
-            Create account? <Link className="l" to={"/register"}>Signup</Link>
+              Create account?{" "}
+              <Link className="l" to={"/register"}>
+                Signup
+              </Link>
             </p>
           </form>
         </div>
